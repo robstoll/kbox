560f3dd2266625ba18396a05602b73313f06ee42
Robert Stoll <rstoll@tutteli.ch>
2018-05-10 19:08:45 +0200

    detect cycles for mapParents and provide option to fail

--- kbox-common/src/main/kotlin/ch/tutteli/kbox/mapParents.kt
+++ kbox-common/src/main/kotlin/ch/tutteli/kbox/mapParents.kt
@@ -6,12 +6,25 @@ package ch.tutteli.kbox
  *
  * A key represent a child and the value its parent.
  *
+ * @param child Defines the starting point of the search.
+ * @param failIfCyclic `false` per default but can be set to `true` in which case it throws an [IllegalStateException]
+ *   if a cycle is detected.
+ *
+ * @throws IllegalStateException In case [failIfCyclic] is `true` and a cycle is detected.
+ *
  * @return The collected parents in order.
  */
-fun <T> Map<T, T>.mapParents(child: T): LinkedHashSet<T> {
+fun <T> Map<T, T>.mapParents(child: T, failIfCyclic: Boolean = false): LinkedHashSet<T> {
     val set = linkedSetOf<T>()
     var parent = this[child]
     while (parent != null) {
+        if (parent == child || set.contains(parent)) {
+            if (failIfCyclic) {
+                val steps = if (set.isEmpty()) "->" else set.joinToString(" -> ", prefix = "-> ", postfix = " ->")
+                throw IllegalStateException("cycle detected: $child $steps $parent")
+            }
+            break
+        }
         set.add(parent)
         parent = this[parent]
     }
--- kbox-jvm/src/test/kotlin/ch/tutteli/kbox/MapToParentsSpec.kt
+++ kbox-jvm/src/test/kotlin/ch/tutteli/kbox/MapToParentsSpec.kt
@@ -1,13 +1,10 @@
 package ch.tutteli.kbox
 
-import ch.tutteli.atrium.api.cc.en_UK.containsStrictly
-import ch.tutteli.atrium.api.cc.en_UK.isEmpty
+import ch.tutteli.atrium.api.cc.en_UK.*
 import ch.tutteli.atrium.assert
+import ch.tutteli.atrium.expect
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.SpecBody
-import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.*
 
 object MapToParentsSpec : Spek({
 
@@ -50,7 +47,7 @@ object MapToParentsSpec : Spek({
         }
     }
 
-    given("two child parent grandparent relation") {
+    given("one child parent and one parent grandparent relation") {
         val map = mapOf(child to parent, parent to grandparent)
 
         testNonExistingChild(map)
@@ -94,6 +91,149 @@ object MapToParentsSpec : Spek({
                 assert(result).containsStrictly(parent)
             }
         }
+    }
 
+    given("one child to child relation (cycle)") {
+        val map = mapOf(child to child)
+
+        context("nothing passed for failIfCyclic (= false)") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("returns an empty set") {
+                    val result = map.mapParents(child)
+                    assert(result).isEmpty()
+                }
+            }
+        }
+
+        context("true passed for failIfCyclic") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("throws an IllegalStateException showing the cycle child -> child") {
+                    expect {
+                        map.mapParents(child, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$child -> $child") }
+                    }
+                }
+            }
+        }
+    }
+
+    given("one child to parent and one parent to child relation (cycle)") {
+        val map = mapOf(child to parent, parent to child)
+
+        context("nothing passed for failIfCyclic (= false)") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("returns parent") {
+                    val result = map.mapParents(child)
+                    assert(result).containsStrictly(parent)
+                }
+            }
+
+            on("passing parent") {
+                it("returns child") {
+                    val result = map.mapParents(parent)
+                    assert(result).containsStrictly(child)
+                }
+            }
+        }
+
+        context("true passed for failIfCyclic") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("throws an IllegalStateException showing the cycle child -> parent -> child") {
+                    expect {
+                        map.mapParents(child, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$child -> $parent -> $child") }
+                    }
+                }
+            }
+
+            on("passing parent") {
+                it("throws an IllegalStateException showing the cycle parent -> child -> parent") {
+                    expect {
+                        map.mapParents(parent, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$parent -> $child -> $parent") }
+                    }
+                }
+            }
+        }
+    }
+
+    given("one child to parent, parent to grandparent and one grandparent to child relation (cycle)") {
+        val map = mapOf(child to parent, parent to grandparent, grandparent to child)
+
+        context("nothing passed for failIfCyclic (= false)") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("returns parent and grandparent") {
+                    val result = map.mapParents(child)
+                    assert(result).containsStrictly(parent, grandparent)
+                }
+            }
+
+            on("passing parent") {
+                it("returns grandparent and child") {
+                    val result = map.mapParents(parent)
+                    assert(result).containsStrictly(grandparent, child)
+                }
+            }
+
+            on("passing grandparent") {
+                it("returns child and parent") {
+                    val result = map.mapParents(grandparent)
+                    assert(result).containsStrictly(child, parent)
+                }
+            }
+        }
+
+        context("true passed for failIfCyclic") {
+
+            testNonExistingChild(map)
+
+            on("passing child") {
+                it("throws an IllegalStateException showing the cycle child -> parent -> grandparent -> child") {
+                    expect {
+                        map.mapParents(child, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$child -> $parent -> $grandparent -> $child") }
+                    }
+                }
+            }
+
+            on("passing parent") {
+                it("throws an IllegalStateException showing the cycle parent -> grandparent -> child -> parent") {
+                    expect {
+                        map.mapParents(parent, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$parent -> $grandparent -> $child -> $parent") }
+                    }
+                }
+            }
+
+            on("passing grandparent") {
+                it("throws an IllegalStateException showing the cycle grandparent -> child -> parent -> grandparent") {
+                    expect {
+                        map.mapParents(grandparent, failIfCyclic = true)
+                    }.toThrow<IllegalStateException>{
+                        message { contains("$grandparent -> $child -> $parent -> $grandparent") }
+                    }
+                }
+            }
+        }
     }
 })
