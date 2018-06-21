d8fd343294dd0aec2c08e0b6704fb56d855b1f8b
Robert Stoll <rstoll@tutteli.ch>
2018-06-21 15:39:57 +0200

    fix IsNotNullAndNotSpec, tests where not carried out

--- kbox-jvm/src/test/kotlin/ch/tutteli/kbox/IsNotNullAndNotSpec.kt
+++ kbox-jvm/src/test/kotlin/ch/tutteli/kbox/IsNotNullAndNotSpec.kt
@@ -4,10 +4,10 @@ import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.assert
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.given
 
 object IsNotNullAndNotSpec : Spek({
-    describe("${CharSequence?::isNotNullAndNotEmpty}") {
+
+    describe("fun ${CharSequence?::isNotNullAndNotEmpty.name}") {
         listOf<Triple<String, CharSequence?, Boolean>>(
             Triple("null", null, false),
             Triple("empty String", "", false),
@@ -17,24 +17,24 @@ object IsNotNullAndNotSpec : Spek({
             Triple("String \"hello\"", "hello", true),
             Triple("StringBuilder \"hello\"", StringBuilder("hello"), true)
         ).forEach { (description, value, expected) ->
-            given("$description, expect $expected") {
+            test("given $description, expect $expected") {
                 assert(value.isNotNullAndNotEmpty()).toBe(expected)
             }
         }
     }
 
-    describe("${CharSequence?::isNotNullAndNotBlank}") {
+    describe("fun ${CharSequence?::isNotNullAndNotBlank.name}") {
         listOf<Triple<String, CharSequence?, Boolean>>(
             Triple("null", null, false),
             Triple("empty String", "", false),
             Triple("empty StringBuilder", StringBuilder(), false),
-            Triple("String \"   \"", StringBuilder("hello"), false),
+            Triple("String \"   \"", StringBuilder("   "), false),
             Triple("StringBuilder \"   \"", StringBuilder("   "), false),
             Triple("String \"hello\"", "hello", true),
             Triple("StringBuilder \"hello\"", StringBuilder("hello"), true)
         ).forEach { (description, value, expected) ->
-            given("$description, expect $expected") {
-                assert(value.isNotNullAndNotEmpty()).toBe(expected)
+            test("given $description, expect $expected") {
+                assert(value.isNotNullAndNotBlank()).toBe(expected)
             }
         }
     }
