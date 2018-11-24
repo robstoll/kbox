8c929f3acf84b9b51943204c59eb8c1c17ca444f
Robert Stoll <rstoll@tutteli.ch>
2018-11-24 22:39:59 +0100

    fix kdoc of JvmSynchronized in js platform use all possible targets

--- kbox-js/src/main/kotlin/ch/tutteli/kbox/JvmSynchronized.kt
+++ kbox-js/src/main/kotlin/ch/tutteli/kbox/JvmSynchronized.kt
@@ -1,8 +1,8 @@
 package ch.tutteli.kbox
 
 /**
- * Typealias for `kotlin.jvm.Synchronized`.
+ * Without any effects on the JS Platform.
  */
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.SOURCE)
 actual annotation class JvmSynchronized actual constructor()
