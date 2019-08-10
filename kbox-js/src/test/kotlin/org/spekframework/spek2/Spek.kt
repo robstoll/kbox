8b0561ae6b9a79588d4b5d9b30bf69697a59318c
Robert Stoll <rstoll@tutteli.ch>
2019-08-10 16:45:01 +0200

    update to spek 2.0.6, remove dummy js implementation for spek

--- kbox-js/src/test/kotlin/org/spekframework/spek2/Spek.kt
+++
@@ -1,7 +0,0 @@
-//TODO remove as soon as https://github.com/spekframework/spek/issues/706 is fixed
-package org.spekframework.spek2
-
-interface Root
-
-@Suppress("UnnecessaryAbstractClass")
-abstract class Spek(val root: Root.() -> Unit)
