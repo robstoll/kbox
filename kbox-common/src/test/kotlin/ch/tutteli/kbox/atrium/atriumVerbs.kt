57cc621f7a77c3245fc7ee523a2b64f8b434009f
Robert Stoll <rstoll@tutteli.ch>
2020-02-03 18:53:16 +0100

    update atrium to 0.9.0 and perform migrations

--- kbox-common/src/test/kotlin/ch/tutteli/kbox/atrium/atriumVerbs.kt
+++ kbox-common/src/test/kotlin/ch/tutteli/kbox/atrium/atriumVerbs.kt
@@ -2,25 +2,20 @@
 
 package ch.tutteli.kbox.atrium
 
-import ch.tutteli.atrium.core.newReportingPlantNullable
-import ch.tutteli.atrium.creating.Assert
-import ch.tutteli.atrium.domain.builders.AssertImpl
-import ch.tutteli.atrium.reporting.reporter
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.reporting.ExpectBuilder
 import ch.tutteli.atrium.reporting.translating.StringBasedTranslatable
 import ch.tutteli.kbox.atrium.AssertionVerb.ASSERT
-import ch.tutteli.kbox.atrium.AssertionVerb.EXPECT_THROWN
 
-internal fun <T : Any> assert(subject: T) = AssertImpl.coreFactory.newReportingPlant(ASSERT, { subject }, reporter)
+internal fun <T> assert(subject: T): Expect<T> =
+    ExpectBuilder.forSubject(subject)
+        .withVerb(ASSERT)
+        .withoutOptions()
+        .build()
 
-internal fun <T : Any> assert(subject: T, createAssertions: Assert<T>.() -> Unit) =
-    AssertImpl.coreFactory.newReportingPlantAndAddAssertionsCreatedBy(ASSERT, { subject }, reporter, createAssertions)
-
-internal fun <T : Any?> assert(subject: T) =
-    AssertImpl.coreFactory.newReportingPlantNullable(ASSERT, { subject }, reporter)
-
-internal fun expect(act: () -> Unit) = AssertImpl.throwable.thrownBuilder(EXPECT_THROWN, act, reporter)
+internal fun <T : Any> assert(subject: T, createAssertions: Expect<T>.() -> Unit) =
+    assert(subject).addAssertionsCreatedBy(createAssertions)
 
 internal enum class AssertionVerb(override val value: String) : StringBasedTranslatable {
     ASSERT("assert"),
-    EXPECT_THROWN("expect the thrown exception"),
 }
