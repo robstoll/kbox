874285870987bc377e78bb97e8732f03d7287b49
Robert Stoll <rstoll@tutteli.ch>
2021-04-01 21:33:58 +0200

    use RootExpectBuilder instead of ExpectBuilder (since deprecated)

--- kbox-common/src/test/kotlin/ch/tutteli/kbox/atrium/atriumVerbs.kt
+++ kbox-common/src/test/kotlin/ch/tutteli/kbox/atrium/atriumVerbs.kt
@@ -1,14 +1,12 @@
-@file:Suppress("MatchingDeclarationName")
-
 package ch.tutteli.kbox.atrium
 
 import ch.tutteli.atrium.creating.Expect
-import ch.tutteli.atrium.domain.builders.reporting.ExpectBuilder
+import ch.tutteli.atrium.logic.creating.RootExpectBuilder
 import ch.tutteli.atrium.reporting.translating.StringBasedTranslatable
 import ch.tutteli.kbox.atrium.AssertionVerb.ASSERT
 
 internal fun <T> assert(subject: T): Expect<T> =
-    ExpectBuilder.forSubject(subject)
+    RootExpectBuilder.forSubject(subject)
         .withVerb(ASSERT)
         .withoutOptions()
         .build()
