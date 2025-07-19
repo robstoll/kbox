b6a4c159c1fa5da3afbb39336bd02ab120a0103f
Robert Stoll <robert.stoll@tegonal.com>
2025-07-19 21:48:52 +0200

    add letIf

--- src/commonMain/kotlin/ch/tutteli/kbox/takeIf.kt
+++ src/commonMain/kotlin/ch/tutteli/kbox/takeIf.kt
@@ -1,12 +1,23 @@
 package ch.tutteli.kbox
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * Calls the [provider] only if the [predicate] holds, returns `null` otherwise.
  *
- * This function shall complement `takeIf` from the stdlib of Kotlin for cases where the cost to call the provider
+ * This function shall complement [kotlin.takeIf] for cases where the cost to call the provider
  * is high or the code involved to define the provider is long and a postfix `takeIf` is less readable
  * than stating it at the beginning.
  *
  * @since 1.1.0
  */
-inline fun <R> takeIf(predicate: Boolean, provider: () -> R): R? = if (predicate) provider() else null
+//TODO move to xIf with 4.0.0
+@OptIn(ExperimentalContracts::class)
+inline fun <R> takeIf(predicate: Boolean, provider: () -> R): R? {
+    contract {
+        callsInPlace(provider, InvocationKind.AT_MOST_ONCE)
+    }
+    return if (predicate) provider() else null
+}
