faffa62180cb3e151fc19150a9639d278a2b39bb
Robert Stoll <robert.stoll@tegonal.com>
2026-09-12 22:42:10 +0200

    prepare dev cycle for v4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.nio.file.StandardCopyOption
 buildscript {
     // needs to be defined in here because otherwise tutteli-publish plugin does not have this information when applied
     // and we use/apply it in the conventions
-    rootProject.version = "3.4.0"
+    rootProject.version = "4.0.0-SNAPSHOT"
     rootProject.group = "ch.tutteli.kbox"
     rootProject.description = "A utility library for Kotlin"
     extra.set("generationFolder", project.files("src/commonMain/generated/kotlin"))
